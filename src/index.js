d50da98d3011386607e07b1e0a11f24d876627ee
willy-10225 <swilly10225@gmail.com>
2022-11-18 17:20:03 +0800

    剛開始

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+import React from "react"
+
+import ReactDOM from 'react-dom'
+
+// ReactDOM.render(<div>
+//     <b>1111111</b>
+//     <ul>
+//         <li>111</li>
+//         <li>111</li>
+//         <li>111</li>
+//         <li>111</li>
+//     </ul>
+//     </div>,document.getElementById("root"))
+
+ReactDOM.render(React.createElement("div",{
+    id:"aaa",
+    class:"bbb"
+},"11111111"),document.getElementById("root"))
